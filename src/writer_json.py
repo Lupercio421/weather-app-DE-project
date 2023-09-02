a591453e08e3e67291d2ed97d1ce5743f0bb0cff
Daniel L <daniel.llupercio@gmail.com>
2023-09-02 16:24:39 -0400

    print statements for the get weather function

--- src/writer_json.py
+++ src/writer_json.py
@@ -15,6 +15,7 @@ def get_weather(location, conn):
 
     # Take only the current part of the JSON
     current = r_string['current']
+    #print(current)
 
     # Fix time format from YYYY-MM-DD hh:mm:ss to -> YYYY-MM-DDThh:mm:ssZ
     # create datetime object from string
@@ -24,6 +25,7 @@ def get_weather(location, conn):
     # Keep in mind this will create timestamps in zulu time. The api only sends you local time.
     # We should fix this, but I can't be bothered right now
     current['last_updated'] = origin_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+    #print(current['last_updated'])
 
     #print(current)  
     # write the weather data to tdengine
@@ -37,12 +39,12 @@ def open_con():
  
     # Open a connection to tdengine cloud. Use the url and token specified in the instance
     try:
-        conn = taosrest.connect(url="https://gw.us-east-1.aws.cloud.tdengine.com",
-                    token="aa69a6e75f27bc2c6eda06012013c26d5b36ffad"
+        conn = taosrest.connect(url="https://gw.us-east.azure.cloud.tdengine.com",
+                    token="74471da90c678dc9ba70fc59f93c53fdd59bbf8f"
                     )
  
     except taosrest.Error as e:
-        print(e)
+        print("error message 1"+ e.errno + "error message 2" + e.msg + "error message 3" + e.status)
 
     return conn
 
@@ -56,7 +58,7 @@ def write_weather(location, weather_js, conn):
     # print(no_whitepsace_location)
 
     # write measurement to tdengine
-    conn.query(f"insert into weather.{no_whitepsace_location} values ('{weather_js['last_updated']}', {weather_js['temp_c']}, {weather_js['temp_f']}, {weather_js['wind_mph']}, {weather_js['wind_kph']})")     
+    conn.query(f"insert into weather.{no_whitepsace_location} values ('{weather_js['last_updated']}', {weather_js['temp_f']}, {weather_js['feelslike_f']}, {weather_js['precip_in']}, {weather_js['wind_mph']})")     
     
 # closes the tdengine connection
 def close_con(conn):
@@ -69,11 +71,11 @@ if __name__ == "__main__":
     # open connection
     conn = open_con()
     
-    # write data for berlin
-    get_weather("berlin",conn)
+    # write data for cuenca
+    get_weather("cuenca",conn)
 
-    # write data for san francisco
-    get_weather("san francisco",conn)
+    # write data for cartagena
+    get_weather("cartagena",conn)
 
     # close connection and end the program
     close_con(conn)
